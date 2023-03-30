5b16a167b63e108e4a79c477329ece0e2109d89f
liz <lissete.trejo@gmail.com>
2023-03-30 12:23:41 -0700

    refactoring to use list/dict comprehension and if ternary operator

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -27,24 +27,21 @@ def watch_movie(user_data, title):
 def get_watched_avg_rating(user_data):
 	watched = user_data['watched']
 
-	if watched:
-		total_rating = sum([movie["rating"] for movie in watched])
-		return total_rating / len(watched)
+	avg = sum([movie["rating"] for movie in watched]) / len(watched) \
+		if watched else 0.0
 	
-	return 0.0
+	return avg
 
 def get_most_watched_genre(user_data):
-	if user_data['watched']:
-		genre_count = {}
-		
-		for movie in user_data['watched']:
-			genre = movie["genre"]
-			genre_count[genre] = genre_count.get(genre,0)+1
-			
-		most_watched = max(genre_count, key=genre_count.get)
-		return most_watched
+	genre_count = {}
 	
-	return None
+	for movie in user_data['watched']:
+		genre_count[movie["genre"]] = genre_count.get(movie["genre"], 0) + 1
+		
+	most_watched =  max(genre_count, key=genre_count.get) \
+		if user_data['watched'] else None
+
+	return most_watched
 
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
@@ -53,19 +50,11 @@ def get_most_watched_genre(user_data):
 def get_unique_watched_movies(user_data, unique_wanted):
 	watched = set([tuple(movie.items()) for movie in user_data['watched']])
 
-	friends_watched = set([
-		tuple(movie.items()) 
-		for friend in user_data['friends'] 
-		for movie in friend['watched']
-	])
+	friends_watched = set([ tuple(movie.items()) \
+		for friend in user_data['friends'] for movie in friend['watched']])
 
-	if unique_wanted == 'user':
-		unique_watched = watched - friends_watched
-	elif unique_wanted == 'friends':
-		unique_watched = friends_watched - watched
-	else:
-		raise ValueError("Invalid watched_type parameter. "
-                        "Must be 'user' or 'friends'.")
+	unique_watched = watched - friends_watched if unique_wanted == 'user' \
+		else friends_watched - watched
 
 	return [dict(movie) for movie in unique_watched]
 
@@ -80,36 +69,30 @@ def get_friends_unique_watched(user_data):
 # -----------------------------------------
 
 def get_available_recs(user_data):
-	recommendation = []
 	friends_unique_watched = get_friends_unique_watched(user_data)
-	
-	for movie in friends_unique_watched:
-		if movie["host"] in user_data["subscriptions"]:
-			recommendation.append(movie)
-			
-	return recommendation
+
+	recommendation = [movie for movie in friends_unique_watched \
+		if movie["host"] in user_data["subscriptions"]]
+
+	return recommendation or []
 
 # -----------------------------------------
 # ------------- WAVE 5 --------------------
 # -----------------------------------------
 
 def get_new_rec_by_genre(user_data):
-	recommendation = []
 	most_watched = get_most_watched_genre(user_data)
 	friends_unique_movies = get_friends_unique_watched(user_data)
 	
-	for movie in friends_unique_movies:
-		if movie["genre"] == most_watched:
-			recommendation.append(movie)
-			
-	return recommendation
+	recommendation =  [movie for movie in friends_unique_movies \
+		if movie["genre"] == most_watched]
+	
+	return recommendation or []
 
 def get_rec_from_favorites(user_data):
-	recommendation = []
 	user_unique_watched = get_unique_watched(user_data)
+
+	recommendation = [movie for movie in user_data["favorites"] \
+		if movie in user_unique_watched]
 	
-	for movie in user_data["favorites"]:
-		if movie in user_unique_watched:
-			recommendation.append(movie)
-			
-	return recommendation
+	return recommendation or []
